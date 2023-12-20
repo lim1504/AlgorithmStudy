9bc5e047a74bd8e8343eeda594ea30c6b0c901c4
lim4158 <lim4158@gmail.com>
2023-12-20 09:31:35 +0900

    [Create] 프로그래머스 day 4

---
+++ src/main/java/testcode/Test20.java
@@ -0,0 +1,18 @@
+package testcode;
+
+/**
+ * 경우의 수는 2가지. true, false
+ * 어렵게 풀 이유가 없다.
+ * if 문 혹은 3항 연산으로 풀이
+ */
+public class Test20 {
+    public static void main(String[] args) {
+        int answer = 0;
+        int a = 0;
+        int b = 0;
+        boolean flag = false;
+
+        answer = flag ? a + b : a - b;
+
+    }
+}
